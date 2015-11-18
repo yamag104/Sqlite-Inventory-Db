02da2a720acd9512603cc057494b061f94b65692
Yoko Yamaguchi <yokoy@Yokos-MacBook-Air.local>
2015-11-17 16:59:37 -0800

    initial commit

---
+++ src/yamag104/Main.java
@@ -0,0 +1,34 @@
+package yamag104;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /**
+         * 1) Print the description, price, and quantity for a provided item ID
+         2) Create a new order (an order consist of a customer and # of items)
+         3) Print the price and description for a given order
+         4) Print the order description for a given customer
+         5) Print the item count and order total for all customers.
+         6) Print the item count and order total per customer with a total greater than $100.
+         7) Insert new items
+         8) Update inventory count for specific item
+         9) Update the item description
+         10) Delete items from inventory
+         11) Exit
+         * */
+
+        System.out.println("1) Print the description, price, and quantity for a provided item ID");
+        System.out.println("2) Create a new order (an order consist of a customer and # of items)");
+        System.out.println("3) Print the price and description for a given order");
+        System.out.println("4) Print the order description for a given customer");
+        System.out.println("5) Print the item count and order total for all customers.");
+        System.out.println("6) Print the item count and order total per customer with a total greater than $100.");
+        System.out.println("7) Insert new items");
+        System.out.println("8) Update inventory count for specific item");
+        System.out.println("9) Update the item description");
+        System.out.println("10) Delete items from inventory");
+        System.out.println("11) Exit");
+
+    }
+}
